a7d1555e4fd0fc4712a9e2987e92df1d382eb5d3
Cleber Duarte <135433935+DuarT3ZiiN@users.noreply.github.com>
2024-09-19 23:25:50 +0000

    atualizacao 1

--- Jogo/jogo.c
+++ Jogo/jogo.c
@@ -3,30 +3,104 @@
 #include <time.h.>
 
 int main(){
-    int n, tentativa;
-    srand(time(NULL));
+    int n, tentativa, totalPartidas = 0, partidasGanhas = 0, tentativasPartida, totalTentativasVitorias;
+    char jogarAgain;
 
-    int numero_aleatorio = rand() % 100 + 1;
+    srand(time(NULL)); // Inicializa o gerador de números aleatorios
 
-    printf("--------------------------------");
-    printf("      JOGO DE ADIVINHACAO       ");
-    printf("--------------------------------");
+do{
+    int numAleatorio = rand() % 100 + 1; // Gera um número aleatorio
+        tentativa = 0;
+        tentativasPartida = 0;
+    int acerto = 0; // variavel para verificação de acerto
 
-    printf("Digite um numero entre 1 e 100:");
+        printf("--------------------------------\n");
+        printf("      JOGO DE ADIVINHACAO       \n");
+        printf("--------------------------------\n");
 
-    if(tentativa >= 0 && tentativa <= 10){
-
-        do{
+    while (tentativa < 10) {
+        printf("Digite um numero entre 1 e 100: \n");
             scanf("%d", &n);
-            if(n < numero_aleatorio){
-                printf("O numero e menor que o numero aleatorio.");
+            tentativa++;
+            tentativasPartida++;
+
+            if(n < numAleatorio){
+                printf("O numero e menor que o numero aleatorio.\n");
+            } else if (n > numAleatorio) {
+                printf("O numero e maior que o numero aleatorio.\n");
             } else {
-                printf("O numero e maior que o numero aleatorio.");
+                printf("Parabens! Voce acertou o numero com %d tentativas!\n", tentativa);
+                partidasGanhas++;
+                totalTentativasVitorias += tentativasPartida;
+                acerto = 1; // adiciona o acerto do jogador
+                break;
             }
-        } while(n =! numero_aleatorio);
+    }
+
+    if(!acerto) {
+        printf("Acabaram suas tentativas! O numero era %d.\n", numAleatorio);
+    }
+
+    total_partidas++;
+
+
+        printf("\nDeseja jogar novamente? (s/n): ");    // Pergunta se o jogador deseja jogar novamente
+        getchar();
+        scanf("%c", &jogar_novamente);
+
+    } while (jogar_novamente == 's' || jogar_novamente == 'S');
+
+    
+        int x, y;
+        char operacao;
+        printf("\nDeseja fazer um calculo? (s/n): ");// Oferece ao jogador a opção de fazer um cálculo matemático
+        getchar();
+        scanf("%c", &operacao);
+        if (operacao == 's' || operacao == 'S') {
+            printf("Escolha a operacao ( soma = 1, subtração = 2, multiplicação = 3, divisão = 4 ): ");
+            getchar();
+            scanf("%c", &operacao);
+            printf("Digite o primeiro numero: ");
+            scanf("%d", &x);
+            printf("Digite o segundo numero: ");
+            scanf("%d", &y);
+
+            switch (operacao) {
+                case '1':
+                    printf("Resultado: %d + %d = %d\n", x, y, x + y);// o resultado da soma com o calculo junto
+                    break;
+                case '2':
+                    printf("Resultado: %d - %d = %d\n", x, y, x - y);// o resultado da subtração com o calculo junto
+                    break;
+                case '3':
+                    printf("Resultado: %d * %d = %d\n", x, y, x * y);// o resultado da multiplicação com o calculo junto
+                    break;
+                case '4':
+                    if (y != 0) {
+                        printf("Resultado: %d / %d = %.2f\n", x, y, (float)x / y); // o resultado da divisão com o calculo junto
+                    } else {
+                        printf("Erro: Divisao por zero nao permitida.\n");
+                    }
+                    break;
+                default:
+                    printf("Operacao invalida.\n");
+                    break;
+            }
+        }
+
+    printf("\n-----------------------------\n");        // Exibe as estatísticas das partidas jogadas
+    printf("       ESTATISTICAS          \n");
+    printf("-----------------------------\n");
+    printf("Total de partidas jogadas: %d\n", total_partidas);
+    printf("Total de partidas ganhas: %d\n", partidas_ganhas);
+    if (partidas_ganhas > 0) {
+        printf("Media de tentativas nas partidas ganhas: %.2f\n", (float)total_tentativas_vitoria / partidas_ganhas);
     } else {
-        printf("Acabou suas tentativas!");
-    }   
+        printf("Nenhuma partida foi vencida.\n");
+    }
+
+    printf("Obrigado por jogar!\n");
+
 
     return 0;
 }
